8a04da3900392efcafb9c8fd13570859c909a7c0
Carlosgarciare2 <122974615+Carlosgarciare2@users.noreply.github.com>
2023-04-26 19:53:08 +0000

    Falta poner colores a los simbolos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,13 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let palo = ["♦", "♥", "♠", "♣"];
+
+  let numberRandom = num[Math.floor(Math.random() * num.length)];
+  let paloRandom = palo[Math.floor(Math.random() * palo.length)];
+
+  document.querySelector("#paloUp").innerHTML = paloRandom;
+  document.querySelector("#paloDown").innerHTML = paloRandom;
+  document.querySelector("#number").innerHTML = numberRandom;
 };
